05e03d90367df59046c4c3f0ee576ac3ef5798f6
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-07-15 19:40:57 +0700

    drivers: usb_bam: Fix misleading indentation warnings
    Fixes the following warnings by GCC:
    
    ../drivers/platform/msm/usb_bam.c:1193:4: warning: misleading indentation; statement is not part of the previous 'else' [-Wmisleading-indentation]
    
    	                        spin_unlock(&ctx->usb_bam_lock);
    	                        ^
    
    ../drivers/platform/msm/usb_bam.c:1191:3: note: previous statement is here
    
    	                else
    	                ^
    ../drivers/platform/msm/usb_bam.c:1316:4: warning: misleading indentation; statement is not part of the previous 'else' [-Wmisleading-indentation]
    
    	                        spin_unlock(&ctx->usb_bam_lock);
    	                        ^
    
    ../drivers/platform/msm/usb_bam.c:1314:3: note: previous statement is here
    
    	                else
    	                ^
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/platform/msm/usb_bam.c
+++ drivers/platform/msm/usb_bam.c
@@ -1153,9 +1153,10 @@ static int usb_bam_disconnect_ipa_prod(
 		if (ctx->pipes_enabled_per_bam == 0)
 			log_event_err("%s: wrong pipes enabled counter for bam=%d\n",
 				__func__, pipe_connect->bam_type);
-		else
+		else {
 			ctx->pipes_enabled_per_bam -= 1;
 			spin_unlock(&ctx->usb_bam_lock);
+		}
 	}
 
 	return 0;
@@ -1274,9 +1275,10 @@ retry:
 		if (ctx->pipes_enabled_per_bam == 0)
 			log_event_err("%s: wrong pipes enabled counter for bam=%d\n",
 				 __func__, pipe_connect->bam_type);
-		else
+		else {
 			ctx->pipes_enabled_per_bam -= 1;
 			spin_unlock(&ctx->usb_bam_lock);
+		}
 	}
 
 	pipe_connect->ipa_clnt_hdl = -1;
